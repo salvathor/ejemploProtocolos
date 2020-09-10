02ca8d0e2327ee02dd6f5bf94d640166f9600864
Salvador Lopez Camacho <salvatore.isc@gmail.com>
2020-09-10 14:47:47 -0500

    Initial Commit

---
+++ ejemploProtocolos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ejemploProtocolos
+//
+//  Created by Salvador Lopez on 10/09/20.
+//  Copyright © 2020 Salvador Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
